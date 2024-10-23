a48709097517237d8fd9adf85f999492f94e9bf9
Maciej Trzuskowski <miketrusky270696@gmail.com>
2024-10-23 11:22:31 +0200

    Showing 3 tables: current, previous & planned activities: changes in controller and view (using partial view).

--- Controllers/ActivitiesController.cs
+++ Controllers/ActivitiesController.cs
@@ -1,5 +1,6 @@
 ﻿using JournalToDoMix.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace JournalToDoMix.Controllers
 {
@@ -15,8 +16,57 @@ namespace JournalToDoMix.Controllers
         }
         public IActionResult Index()
         {
-            var actions = _dbContext.Activities.ToList();
-            return View(actions);
+            var now = DateTime.Now;
+
+            UpdateActivitiesCompletedStatus(now);
+
+            var plannedActivities = _dbContext.Activities
+                .Where(a => a.StartedAt > now)
+                .AsNoTracking()
+                .ToList();
+
+            var currentActivities = _dbContext.Activities
+                .Where(a => a.StartedAt <= now && !a.IsCompleted)
+                .AsNoTracking()
+                .AsEnumerable()
+                .Where(a => a.StartedAt.Add(a.DurationPlanned) >= now)
+                .ToList();
+
+            var previousActivities = _dbContext.Activities
+                .Where(a => a.IsCompleted ||
+                            (!plannedActivities.Select(p => p.Id).Contains(a.Id) &&
+                             !currentActivities.Select(c => c.Id).Contains(a.Id)))
+                .AsNoTracking()
+                .ToList();
+
+            ViewBag.PlannedActivities = plannedActivities;
+            ViewBag.CurrentActivities = currentActivities;
+            ViewBag.PreviousActivities = previousActivities;
+
+            return View();
+        }
+
+        private void UpdateActivitiesCompletedStatus(DateTime now)
+        {
+            var incompletedActivities = _dbContext.Activities
+                            .Where(a => !a.IsCompleted)
+                            .ToList();
+
+            bool hasChange = false;
+
+            foreach (var activity in incompletedActivities)
+            {
+                var endTime = activity.StartedAt.Add(activity.DurationPlanned);
+                if (endTime < now)
+                {
+                    activity.IsCompleted = true;
+                    _dbContext.Activities.Update(activity);
+                    hasChange = true;
+                }
+            }
+
+            if (hasChange)
+                _dbContext.SaveChanges();
         }
         #region Add new actions
         public IActionResult Add()
