c95f2bfa1f3886f3575cdb801d1f3f7bfeccb64c
Maciej Trzuskowski <miketrusky270696@gmail.com>
2024-11-08 11:19:43 +0100

    Added timeline arrows with changing week function.

--- Controllers/TimelineController.cs
+++ Controllers/TimelineController.cs
@@ -1,5 +1,6 @@
 ﻿using JournalToDoMix.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
 
 namespace JournalToDoMix.Controllers
 {
@@ -13,29 +14,47 @@ namespace JournalToDoMix.Controllers
             _logger = logger;
             _dbContext = dbContext;
         }
-        public IActionResult Index()
+        public IActionResult Index(string? startOfWeek, int daysOfWeekToAdd = 0)
         {
-            var now = DateTime.Now;
-            var startOfWeek = now.AddDays(-(int)now.DayOfWeek + (int)DayOfWeek.Monday); // Poniedziałek bieżącego tygodnia
-            var daysOfWeek = Enumerable.Range(0, 7)
-                                       .Select(offset => startOfWeek.AddDays(offset))
-                                       .ToList();
+            DateTime startOfCurrentWeek;
+            if(ModelState.IsValid && !string.IsNullOrEmpty(startOfWeek) && DateTime.TryParse(startOfWeek, CultureInfo.InvariantCulture, out var parsedData))
+            {
+                startOfCurrentWeek = parsedData;                
+            }
+            else
+            {
+                startOfCurrentWeek = GetStartOfCurrentWeek();
+            }
 
+            startOfCurrentWeek = startOfCurrentWeek.AddDays(daysOfWeekToAdd);
 
-            var activities = _dbContext.Activities
-                .Where(a => a.StartedAt >= startOfWeek && a.StartedAt < startOfWeek.AddDays(7))
-                .ToList();
-
-            var earliestActivitiesByDay = daysOfWeek.ToDictionary(
-                day => day,
-                day => activities.Where(a => a.StartedAt.Date == day.Date)
-                                 .OrderBy(a => a.StartedAt)
-                                 .FirstOrDefault());
-
-            ViewBag.DaysOfWeek = daysOfWeek;
-            ViewBag.EarliestActivitiesByDay = earliestActivitiesByDay;
+            ViewBag.DaysOfWeek = GetDaysOfWeek(startOfCurrentWeek);
+            ViewBag.EarliestActivitiesByDay = GetEarliestActivitiesForWeek(startOfCurrentWeek, ViewBag.DaysOfWeek);
 
             return View();
+        }        
+        private List<DateTime> GetDaysOfWeek(DateTime startOfWeek)
+        {
+            return Enumerable.Range(0, 7)
+                             .Select(offset => startOfWeek.AddDays(offset))
+                             .ToList();
+        }
+        private Dictionary<DateTime, Activity?> GetEarliestActivitiesForWeek(DateTime startOfWeek, List<DateTime> daysOfWeek)
+        {
+            var activities = _dbContext.Activities
+                                       .Where(a => a.StartedAt >= startOfWeek && a.StartedAt < startOfWeek.AddDays(7))
+                                       .ToList();
+
+            return daysOfWeek.ToDictionary(
+                   day => day,
+                   day => activities.Where(a => a.StartedAt.Date == day.Date)
+                                    .OrderBy(a => a.StartedAt)
+                                    .FirstOrDefault());
+        }
+        private DateTime GetStartOfCurrentWeek()
+        {
+            var now = DateTime.Now;
+            return now.AddDays(-(int)now.DayOfWeek + (int)DayOfWeek.Monday);
         }
     }
 }
