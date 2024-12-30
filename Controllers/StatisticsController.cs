30a8438ff3a3b830f0b25bc1bd2cc7bff33f7d78
Maciej Trzuskowski <miketrusky270696@gmail.com>
2024-12-30 12:18:35 +0100

    Added two stats charts: pie chart for activities-count and column chart for activities-time.

--- Controllers/StatisticsController.cs
+++ Controllers/StatisticsController.cs
@@ -1,12 +1,62 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using JournalToDoMix.Models;
+using JournalToDoMix.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace JournalToDoMix.Controllers
 {
     public class StatisticsController : Controller
     {
+        private readonly ApplicationDbContext _dbContext;
+        public StatisticsController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
         public IActionResult Index()
         {
-            return View();
+            return RedirectToAction(nameof(ActivitiesCountChart));
+        }
+        public IActionResult ActivitiesCountChart()
+        {
+            var eachActivityCount = _dbContext.Activities
+                                                .GroupBy(a => a.ActivityTitle)
+                                                .Select(g => new StatisticsViewModel
+                                                {
+                                                    Label = g.Key.Title,
+                                                    Count = g.Count(),
+                                                    Type = ChartType.ActivityCount
+                                                })
+                                                .ToList();
+
+            return View(eachActivityCount);
+        }
+        public IActionResult ActivitiesTimeChart()
+        {
+            var eachActivityTime = _dbContext.Activities
+                                                .GroupBy(a => a.ActivityTitle)         
+                                                .AsEnumerable()
+                                                .Select(g => new StatisticsViewModel
+                                                {
+                                                    Label = g.Key.Title,
+                                                    Count = CalculateHours(g.Sum(a => a.DurationPlanned.Ticks)),
+                                                    Type = ChartType.ActivityTime
+                                                })
+                                                .ToList();
+
+            return View(eachActivityTime);
+        }
+        private double CalculateHours(long time)
+        {
+            var totalHours = TimeSpan.FromTicks(time).TotalHours;
+            var hoursPart = Math.Floor(totalHours);
+            var minutesPart = totalHours - hoursPart;
+            if (minutesPart > 0)
+            {
+                minutesPart *= 100;
+                minutesPart = Math.Floor(minutesPart);
+                minutesPart /= 100;
+            }
+
+            return hoursPart + minutesPart;
         }
     }
 }
---
+++ ViewModels/StatisticsViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace JournalToDoMix.ViewModels
+{
+    public class StatisticsViewModel
+    {
+        public string? Label { get; set; }
+        public double Count { get; set; }
+        public ChartType Type { get; set; }
+    }    
+
+    public enum ChartType
+    {
+        ActivityCount,
+        ActivityTime
+    }
+}
