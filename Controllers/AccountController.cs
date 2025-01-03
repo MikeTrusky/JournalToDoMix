e5e2706035c34c0923a4c28db466620c7d2c7a6c
Maciej Trzuskowski <miketrusky270696@gmail.com>
2025-01-03 15:07:31 +0100

    Improved login & register validation.

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 ﻿using JournalToDoMix.Models;
 using JournalToDoMix.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -15,77 +16,107 @@ namespace JournalToDoMix.Controllers
             _userManager = userManager;
             _signInManager = signInManager;
         }
+
+        #region Login
         public IActionResult Login()
         {
             return View();
         }
+
         [HttpPost]
         public async Task<IActionResult> Login(LoginViewModel loginModel)
         {
             if (!ModelState.IsValid)
-                return View();
+                return View(loginModel);
 
             var user = await _userManager.Users.FirstOrDefaultAsync(x => x.Email == loginModel.Email.ToLower());
             if (user == null)
             {
-                ModelState.AddModelError("", "Invalid login attempt");
+                ModelState.AddModelError("loginError", "Invalid login attempt");
                 return View(loginModel);
             }
 
             var result = await _signInManager.PasswordSignInAsync(user, loginModel.Password, loginModel.RememberMe, false);
 
-            if(result.Succeeded)
-            {                
-                return RedirectToAction("Index", "Timeline");
-            }
-            else
+            if (!result.Succeeded)
             {
-                ModelState.AddModelError("", "Invalid login attempt");
+                ModelState.AddModelError("loginError", "Invalid login attempt");
                 return View(loginModel);
-            }            
+            }
+
+            return RedirectToAction("Index", "Timeline");
         }
+        #endregion
+
+        #region Register
         public IActionResult Register()
-        {                          
+        {
             return View();
         }
         [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
         public async Task<IActionResult> Register(RegisterViewModel registerModel)
         {
             if (!ModelState.IsValid)
                 return View(registerModel);
 
+            if (await UserExists(registerModel.UserName, registerModel.Email))
+                return View(registerModel);
+
             var appUser = new AppUser
             {
                 UserName = registerModel.UserName,
-                Email = registerModel.Email                
+                Email = registerModel.Email
             };
 
-            var createUser = await _userManager.CreateAsync(appUser, registerModel.Password);
+            var createUserResult = await _userManager.CreateAsync(appUser, registerModel.Password);
 
-            if (createUser.Succeeded)
+            if (!createUserResult.Succeeded)
             {
-                var roleResult = await _userManager.AddToRoleAsync(appUser, "User");
-                if (roleResult.Succeeded)
-                {
-                    await _signInManager.SignInAsync(appUser, isPersistent: false);
-                    return RedirectToAction("Index", "Timeline");
-                }                
-                else
-                {
-                    foreach (var error in roleResult.Errors)
-                    {
-                        ModelState.AddModelError("", error.Description);
-                    }
-                }
+                AddErrors(createUserResult, "registerError");
+                return View(registerModel);
             }
 
-            foreach(var error in createUser.Errors)
+            var roleResult = await _userManager.AddToRoleAsync(appUser, "User");
+
+            if (!roleResult.Succeeded)
             {
-                ModelState.AddModelError("", error.Description);
+                AddErrors(roleResult, "registerError");
+                return View(registerModel);
             }
 
-            return View(registerModel);
+            await _signInManager.SignInAsync(appUser, isPersistent: false);
+            return RedirectToAction("Index", "Timeline");
         }
+
+        private async Task<bool> UserExists(string userName, string email)
+        {
+            bool error = false;
+            if (await _userManager.FindByNameAsync(userName) != null)
+            {
+                ModelState.AddModelError("UserName", "This username is already taken.");
+                error = true;
+            }
+
+            if (await _userManager.FindByEmailAsync(email) != null)
+            {
+                ModelState.AddModelError("Email", "This email is already taken.");
+                error = true;
+            }            
+
+            return error;
+        }
+
+        private void AddErrors(IdentityResult result, string modelErrorKey)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(modelErrorKey, error.Description);
+            }
+        }
+        #endregion
+
         [HttpPost]
         public async Task<IActionResult> Logout()
         {
